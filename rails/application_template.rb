1ba11c719749b281af0f9cfb39b23147ca3af1be
Michael Deering <mdeering@mdeering.com>
2015-11-28 19:29:48 -0700

    WIP template does nothing yet

--- rails/application_template.rb
+++ rails/application_template.rb
@@ -10,6 +10,24 @@ Dir.glob("#{PRIMARY_TEMPLATE_PATH}/**/*", File::FNM_DOTMATCH).each do |file|
   copy_file source, force: true
 end
 
+[
+  'app/assets/javascripts/application.js',
+  'app/assets/stylesheets/application.css',
+  'app/helpers/application_helper.rb',
+  'app/views/layouts/application.html.erb'
+].each do |file|
+  remove_file file
+end
+
 after_bundle do
+  # generate 'responders:install'
+  generate 'cucumber:install'
+  generate 'devise:install'
+  generate 'rspec:install'
   generate 'simple_form:install', '--bootstrap'
+
+  rake 'db:migrate'
+
+  run 'guard init inch livereload rails rspec rubocop yardstick'
+  run 'rubocop -D --rails -a'
 end
